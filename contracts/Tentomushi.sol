b7eb83d4171064884be33adef6d527b048c96768
tentomushi20 <inertiadevloper@gmail.com>
2024-07-19 23:43:44 +0530

    Update Tentomushi.sol

--- contracts/Tentomushi.sol
+++ contracts/Tentomushi.sol
@@ -4,55 +4,52 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-contract TentoToken is ERC20{
-    struct oneItem{
+contract TentoToken is ERC20 {
+    struct oneItem {
         uint value;
         string name;
     }
     oneItem[] public alloneItems;
     address owner;
-    mapping (address => oneItem[]) public userReddem;
-    
+    mapping(address => oneItem[]) public userReddem;
+
     constructor() ERC20("TENOTO", "TTO") {
-         owner = msg.sender;      
-         alloneItems.push(oneItem(200,"NFT BATMAN"));   
-         alloneItems.push(oneItem(300,"NFT KALKI"));   
-         alloneItems.push(oneItem(100,"NFT JON"));   
+        owner = msg.sender;
+        alloneItems.push(oneItem(200, "NFT BATMAN"));
+        alloneItems.push(oneItem(300, "NFT KALKI"));
+        alloneItems.push(oneItem(100, "NFT JON"));
     }
 
-
-    function Mints(address toAddress, uint supply) public {
-        require(msg.sender == owner,"Only Owner can min the tokesn");
+    function mint(address toAddress, uint supply) public {
+        require(msg.sender == owner, "Only Owner can mint the tokens");
         _mint(toAddress, supply);
     }
 
-    function Burns(uint supply) public {
-        if(balanceOf(msg.sender) < supply){
-            revert("You Don't have enough Tokens");
+    function burn(uint supply) public {
+        if (balanceOf(msg.sender) < supply) {
+            revert("You don't have enough Tokens");
         }
-
         _burn(msg.sender, supply);
     }
 
-    function getoneItem() public  view returns (oneItem[] memory){
+    function getoneItem() public view returns (oneItem[] memory) {
         return alloneItems;
     }
 
-    function getUserReddem() public  view returns (oneItem[] memory){
+    function getUserReddem() public view returns (oneItem[] memory) {
         return userReddem[msg.sender];
     }
 
-    function NowReddem(uint index) public  {
-        require(index > 0 && index < 3, "Please Enter index in range 0 to 2");
+    function nowReddem(uint index) public {
+        require(index >= 0 && index < 3, "Please enter index in range 0 to 2");
         require(balanceOf(msg.sender) >= alloneItems[index].value, "You don't have enough Tokens");
 
         userReddem[msg.sender].push(alloneItems[index]);
-        Burns(alloneItems[index].value);
+        burn(alloneItems[index].value);
     }
 
-    // function Reddem()
-
-
-
+    function transferTokens(address to, uint256 amount) public {
+        require(amount <= balanceOf(msg.sender), "Not enough tokens");
+        _transfer(msg.sender, to, amount);
+    }
 }
